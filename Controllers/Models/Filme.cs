8974a014cc2400eb9cd985045b2961519d7ff745
Luciane Santos <lucianesantcs@gmail.com>
2022-07-07 21:46:36 -0300

    feat: enviando requisições para a api

---
+++ Controllers/Models/Filme.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+        
+        
+    }
+}
