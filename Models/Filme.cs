9e3f60705433ffd4d19d39d8505218a03d2415cc
Luciane Santos <lucianesantcs@gmail.com>
2022-07-22 17:28:00 -0300

    feat: reotrnando informações da api

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -5,6 +5,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
