ef6e422db951863acfc458b76f63e8962059718b
Luciane Santos <lucianesantcs@gmail.com>
2022-07-22 16:34:26 -0300

    feat: adicionado validação de parâmetros

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -13,7 +13,6 @@ namespace FilmesAPI.Controllers
         public void AdicionaFilme([FromBody] Filme filme)
         {
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
         }
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,14 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
-        
-        
     }
 }
